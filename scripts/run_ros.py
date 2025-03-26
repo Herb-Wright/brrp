a7a105bc2fba186ce4e0134839f7fd80b1588f61
Herbie Wright <herb.e.wright@gmail.com>
2025-03-26 00:11:46 -0600

    add code for marching cubes and publishing mesh

--- scripts/run_ros.py
+++ scripts/run_ros.py
@@ -1,14 +1,17 @@
-import time
+import logging
 
 import torch
 from sensor_msgs.msg import PointCloud2, Image as msg_image
 import ros_numpy
 import rospy
 import numpy as np
+from shape_msgs.msg import Mesh
+import trimesh
 
 from custom_msgs.msg import MeshList
 from brrp.full_method import full_brrp_method
 from brrp.segmenter import GroundedSamSegmenter
+from brrp.visualization import gen_mesh_for_sdf_batch_3d, some_colors
 
 rate = 0.2  # Hz; once ever 5 seconds
 path_to_prior = "./ycb_prior"
@@ -71,10 +74,39 @@ class BRRPNode:
 
         weights, hp_transform = full_brrp_method(rgb, xyz, seg_mask, path_to_prior)
 
-        # TODO: make mesh and publish
+        num_classes = int(seg_mask.amax().item() + 1)
+        def occ_func(x: torch.Tensor) -> torch.Tensor:
+            out = x.to(torch.device("cuda")).to(torch.float32)
+            out = torch.mean(torch.sigmoid(torch.sum(hp_transform.transform(out.unsqueeze(0)) * weights.unsqueeze(2), dim=-1)), dim=0)
+            return out.cpu()
+        meshes: list[trimesh.Trimesh] = []
+        for i in range(0, num_classes-1):
+            occ_func_i = lambda x: occ_func(x)[i]
+            mins = torch.amin(xyz[seg_mask == i+1], dim=0)
+            maxs = torch.amax(xyz[seg_mask == i+1], dim=0)
+            cntr = 0.5 * (mins + maxs).cpu()
+            mesh = gen_mesh_for_sdf_batch_3d(
+                occ_func_i,
+                xlim=[cntr[0] - 0.4, cntr[0] + 0.4], 
+                ylim=[cntr[1] - 0.4, cntr[1] + 0.4], 
+                zlim=[cntr[2] - 0.4, cntr[2] + 0.4], 
+                resolution=0.015,  # change this for better resolution
+                confidence=0.5,
+            )
+            if mesh is None:
+                logging.warning("empty mesh :-(")
+                continue
+            mesh.visual.vertex_colors = some_colors[i] + [215]
+            meshes.append(mesh)
 
         msg = MeshList()
-        msg.meshes = []
+        msg_meshes = []
+        for mesh in meshes:
+            msg_mesh = Mesh()
+            msg_mesh.triangles = mesh.faces
+            msg_mesh.vertices = mesh.vertices
+            msg_meshes.append(msg_mesh)
+        msg.meshes = msg_meshes
         self.pub.publish(msg)
 
 
